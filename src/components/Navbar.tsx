adfdefa27bc23a2940001b120e11bf3fdd4a45a2
N3N3 <ntoloumu@hawaii.edu>
2024-11-20 21:26:50 -1000

    marketplace(RD)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,121 +1,144 @@
-/* eslint-disable react/no-unescaped-entities */
-/* eslint-disable react/jsx-indent */
-/* eslint-disable @typescript-eslint/indent */
-
 'use client';
 
 import { useSession } from 'next-auth/react';
 import { usePathname } from 'next/navigation';
-import { Container, Image, Nav, Navbar, NavDropdown } from 'react-bootstrap';
-import { BoxArrowRight, PersonFill, PersonPlusFill } from 'react-bootstrap-icons';
-// eslint-disable-next-line import/extensions
-import { ComponentIDs } from '@/utilities/ids';
+import { Image, Nav, Navbar, NavDropdown, Form, FormControl, Badge } from 'react-bootstrap';
+import { BoxArrowRight, PersonFill, PersonPlusFill, PersonCircle, Cart3 } from 'react-bootstrap-icons';
+import Link from 'next/link';
 
 const NavBar: React.FC = () => {
   const { data: session } = useSession();
   const pathname = usePathname();
   const currentUser = session?.user?.email;
-  // const userWithRole = session?.user as { email: string; randomKey: string };
-  // const role = userWithRole?.randomKey;
-  const menuStyle = { marginBottom: '0px' };
   const navbarClassName = currentUser ? 'bg-dark' : 'bg-light';
-  // const navbarVariant = currentUser ? 'dark' : 'light';
+  const cartItemCount = 5;
+
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    const form = e.target as HTMLFormElement;
+    const searchInput = form.elements[0] as HTMLInputElement;
+    const searchValue = searchInput.value;
+    console.log('Searching for:', searchValue);
+  };
+
   return (
-    <Navbar expand="lg" style={menuStyle} className={navbarClassName}>
-      <Container>
-        <Navbar.Brand href="/" className="align-items-center">
-          <span style={{ fontWeight: 800, fontSize: '24px' }}>
-            <Image src="/images/logo.png" width={50} style={{ marginBottom: 3 }} alt="Bowfolios" />
-            Bowfolios
-          </span>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls={ComponentIDs.basicNavbarNav} />
-        <Navbar.Collapse id={ComponentIDs.basicNavbarNav}>
-          <Nav className="me-auto justify-content-start">
-            {currentUser ? (
-              <Nav.Link id={ComponentIDs.homeMenuItem} active={pathname === '/home'} href="/home" key="home">
-                Home
+    <>
+      {/* Top Navbar */}
+      <Navbar
+        expanded="lg"
+        style={{ paddingTop: '2px', paddingBottom: '2px', fontSize: '0.9rem', borderBottom: '2px solid #ddd' }}
+        className="bg-secondary"
+      >
+        <div className="container">
+          <Nav className="me-auto">
+            <Link href="/limited-time-offers" passHref>
+              <Nav.Link className="text-muted" style={{ fontSize: '0.8rem', marginRight: '15px' }}>
+                Limited Time Offers
               </Nav.Link>
-            ) : (
-              ''
-            )}
-            <Nav.Link
-              id={ComponentIDs.profilesMenuItem}
-              active={pathname === '/profiles'}
-              href="/profiles"
-              key="profiles"
-            >
-              Profiles
-            </Nav.Link>
-            <Nav.Link
-              id={ComponentIDs.projectsMenuItem}
-              active={pathname === '/projects'}
-              href="/projects"
-              key="projects"
-            >
-              Projects
-            </Nav.Link>
-            <Nav.Link
-              id={ComponentIDs.interestsMenuItem}
-              active={pathname === '/interests'}
-              href="/interests"
-              key="interests"
-            >
-              Interests
-            </Nav.Link>
-            {currentUser
-              ? [
-                  <Nav.Link
-                    id={ComponentIDs.addProjectMenuItem}
-                    active={pathname === '/addProject'}
-                    href="/addProject"
-                    key="addP"
-                  >
-                    Add Project
-                  </Nav.Link>,
-                  <Nav.Link
-                    id={ComponentIDs.filterMenuItem}
-                    active={pathname === '/filter'}
-                    href="/filter"
-                    key="filter"
-                  >
-                    Filter
-                  </Nav.Link>,
-                ]
-              : ''}
-            <Nav.Link
-              id={ComponentIDs.projectsMenuItem}
-              active={pathname === '/lucky'}
-              href="/lucky"
-              key="lucky"
-            >
-              I'm Feeling Lucky
-            </Nav.Link>
+            </Link>
+            <Link href="/giftcards" passHref>
+              <Nav.Link className="text-muted" style={{ fontSize: '0.8rem', marginRight: '15px' }}>
+                Giftcards
+              </Nav.Link>
+            </Link>
+            <Link href="/about-us" passHref>
+              <Nav.Link className="text-muted" style={{ fontSize: '0.8rem', marginRight: '15px' }}>
+                About Us
+              </Nav.Link>
+            </Link>
           </Nav>
-          <Nav className="justify-content-end">
-            {currentUser ? (
-              <NavDropdown id={ComponentIDs.currentUserDropdown} title={currentUser}>
-                <NavDropdown.Item id={ComponentIDs.currentUserDropdownSignOut} href="/auth/signout">
-                  <BoxArrowRight />
-                  Sign out
-                </NavDropdown.Item>
-              </NavDropdown>
-            ) : (
-              <NavDropdown id={ComponentIDs.loginDropdown} title="Login">
-                <NavDropdown.Item id={ComponentIDs.loginDropdownSignIn} href="/auth/signin">
-                  <PersonFill />
-                  Sign in
-                </NavDropdown.Item>
-                <NavDropdown.Item id={ComponentIDs.loginDropdownSignUp} href="/auth/signup">
-                  <PersonPlusFill />
-                  Sign up
-                </NavDropdown.Item>
-              </NavDropdown>
-            )}
+          <Nav className="ms-auto">
+            <Link href="/sell" passHref>
+              <Nav.Link className="text-muted" style={{ fontSize: '0.8rem', marginLeft: '15px' }}>
+                Sell
+              </Nav.Link>
+            </Link>
+            <Link href="/customer-service" passHref>
+              <Nav.Link className="text-muted" style={{ fontSize: '0.8rem', marginLeft: '15px' }}>
+                Customer Service
+              </Nav.Link>
+            </Link>
           </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+        </div>
+      </Navbar>
+
+      {/* Main Navbar */}
+      <Navbar expanded="lg" style={{ borderBottom: '2px solid #ddd' }} className={navbarClassName}>
+        <div className="container">
+          <Navbar.Brand href="/" className="align-items-center">
+            <span style={{ fontWeight: 800, fontSize: '24px' }}>
+              <Image src="/images/logo.png" width={50} alt="Mānoa Now Marketplace" />
+              Mānoa Now Marketplace
+            </span>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbar-nav" />
+          <Navbar.Collapse id="navbar-nav">
+            <Nav className="me-auto">
+              {currentUser && (
+                <Link href="/home" passHref>
+                  <Nav.Link active={pathname === '/home'}>Home</Nav.Link>
+                </Link>
+              )}
+              <Link href="/profiles" passHref>
+                <Nav.Link active={pathname === '/profiles'}>Profiles</Nav.Link>
+              </Link>
+              <Link href="/projects" passHref>
+                <Nav.Link active={pathname === '/projects'}>Projects</Nav.Link>
+              </Link>
+              <Link href="/interests" passHref>
+                <Nav.Link active={pathname === '/interests'}>Interests</Nav.Link>
+              </Link>
+            </Nav>
+
+            {/* Search Bar */}
+            <Form onSubmit={handleSearch}>
+              <FormControl
+                type="search"
+                placeholder="Search..."
+                className="me-2"
+                aria-label="Search"
+              />
+            </Form>
+
+            {/* User and Cart */}
+            <Nav className="justify-content-end">
+              {currentUser ? (
+                <NavDropdown title={currentUser}>
+                  <NavDropdown.Item href="/auth/signout">
+                    <BoxArrowRight />
+                    {' '}
+                    Sign out
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <NavDropdown title={<PersonCircle size={20} />}>
+                  <NavDropdown.Item href="/auth/signin">
+                    <PersonFill />
+                    {' '}
+                    Sign in
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="/auth/signup">
+                    <PersonPlusFill />
+                    {' '}
+                    Sign up
+                  </NavDropdown.Item>
+                </NavDropdown>
+              )}
+
+              {/* Cart Icon */}
+              <Nav.Link href="/cart" className="d-flex align-items-center position-relative">
+                <Cart3 size={20} />
+                {cartItemCount > 0 && (
+                  <Badge pill bg="success" style={{ position: 'absolute', top: '-5px', right: '-5px' }}>
+                    {cartItemCount}
+                  </Badge>
+                )}
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </div>
+      </Navbar>
+    </>
   );
 };
 
