36ca21483c5ca05a06b278ea30287955de34b9e3
alex-cornwell <ac336@hawaii.edu>
2024-12-03 12:13:11 -1000

    update

--- src/lib/ProductCardData.ts
+++ src/lib/ProductCardData.ts
@@ -1,7 +1,7 @@
 export type ProductCardData = {
   name: string;
-  picture: string
-  price: string
-  description: string
-  owner: string
+  picture: string;
+  price: string;
+  description: string;
+  owner: string;
 };
