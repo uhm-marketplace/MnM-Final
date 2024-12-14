09f2a5764d22fb9958b19b35c415b450d037be8a
Lionel Derrick Roxas <lionelroxas80@gmail.com>
2024-12-13 19:22:32 -1000

    add test

---
+++ tests/user-testing.spec.ts
@@ -0,0 +1,20 @@
+import { test } from '@playwright/test';
+
+test.use({
+  storageState: 'lionel-auth.json',
+});
+
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await page.getByRole('button', { name: 'Get Started' }).click();
+  await page.locator('#loginDropdown').click();
+  await page.getByRole('link', { name: 'Sign in' }).click();
+  await page.getByPlaceholder('Enter your email').click();
+  await page
+    .getByPlaceholder('Enter your email')
+    .fill('lionelroxas80@gmail.com');
+  await page.getByPlaceholder('Enter your password').click();
+  await page.getByPlaceholder('Enter your password').fill('100102');
+  await page.getByRole('button', { name: 'Sign in' }).click();
+  await page.goto('http://localhost:3000/home');
+});
